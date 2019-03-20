de66d7b5ce9cd694aa42a8da34242b1a33c49129
Oleksandr Oleksiv <oleksiv1994@gmail.com>
2019-03-20 23:13:24 +0200

    added some dummy functionality

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-//    TODO
+    const url = req.url;
+if (url === '/') {
+    res.write('<html>');
+    res.write('<head><title>Dummy Node Server</title></head>');
+    res.write('<body><form action="/dummy-request" method="POST"><input type="text" name="message"><button type="submit">Click to send a request to server</button></form></body>');
+    res.write('</html>');
+    return res.end;
+}
+
+res.setHeader('Content-Type', 'text/html');
+res.write('<html>');
+res.write('<head><title>Dummy page</title></head>');
+res.write('<body><h1>Hello from Node Server</h1></body>');
+res.write('</html>');
+res.end();
 });
 
 server.listen(3000);
